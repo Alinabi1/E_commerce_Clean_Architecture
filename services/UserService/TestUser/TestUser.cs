1178124c4f9c3be50bdd4fe834caa07142ed66be
Alinabi1 <alinabi@kth.se>
2025-12-21 23:07:03 +0100

    Initial Commit

---
+++ services/UserService/TestUser/TestUser.cs
@@ -0,0 +1,35 @@
+﻿using UserService.Domain.Entities;
+namespace TestUser
+{
+    public class TestUser
+    {
+        [Fact]
+        public void Create_ShouldWork_AllDataValid()
+        {
+            string firstName= "John";
+            string lastName = "Wick";
+            string email = "john.wick@gamil.com";
+            string passwordHash = "Hash";
+            string role = "Customer";
+            User user = new (firstName,lastName,email,passwordHash,role);
+
+            Assert.Equal(firstName, user.FirstName);
+            Assert.Equal(lastName, user.LastName);
+            Assert.Equal(email, user.Email);
+            Assert.Equal(passwordHash, user.PasswordHash);
+            Assert.Equal(UserRole.Customer, user.Role);
+        }
+
+        [Fact]
+        public void Create_ShouldFail_InvalidEmail()
+        {
+            string firstName = "John";
+            string lastName = "Wick";
+            string email = "john.wick-gamil.com";
+            string passwordHash = "Hash";
+            string role = "Customer";
+            var exception = Assert.Throws<ArgumentException>(() => new User(firstName, lastName, email, passwordHash, role));
+            Assert.Equal("Invalid email!", exception.Message);
+        }
+    }
+}
---
+++ services/UserService/UserService.Domain/Entities/User.cs
@@ -0,0 +1,70 @@
+﻿namespace UserService.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; private set; } = string.Empty;
+        public string LastName { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public string PasswordHash { get; private set; } = string.Empty;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public UserRole Role { get; private set; }
+
+        public User() { }
+
+        public User(string firstName, string lastName, string email, string passwordHash, string role)
+        {
+            SetFirstName(firstName);
+            SetLastName(lastName);
+            SetEmail(email);
+            SetPasswordHash(passwordHash);
+            SetRole(role);
+        }
+
+        public void SetFirstName(string firstName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException("First-name can not be empty!");
+            }
+            FirstName = firstName;
+        }
+
+        public void SetLastName(string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException("Last-name can not be empty!");
+            }
+            LastName = lastName;
+        }
+
+        public void SetEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+            {
+                throw new ArgumentException("Invalid email!");
+            }
+            Email = email;
+        }
+
+        public void SetPasswordHash(string passwordHash)
+        {
+            if (string.IsNullOrWhiteSpace(passwordHash))
+            {
+                throw new ArgumentException("Password can not be empty!");
+            }
+            PasswordHash = passwordHash;
+        }
+
+        public void SetRole(string role)
+        {
+            if (!Enum.TryParse<UserRole>(role, true, out var parsedRole) ||
+               !Enum.IsDefined(typeof(UserRole), parsedRole))
+            {
+                throw new ArgumentException("Invalid user role!");
+            }
+            Role = parsedRole;
+        }
+    }
+}
