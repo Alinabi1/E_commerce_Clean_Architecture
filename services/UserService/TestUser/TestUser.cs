dea564fd0e824a8973667068afae0d405e8b17de
Alinabi1 <alinabi@kth.se>
2026-01-12 15:15:54 +0100

    More tests for user class

--- services/UserService/TestUser/TestUser.cs
+++ services/UserService/TestUser/TestUser.cs
@@ -11,6 +11,7 @@ namespace TestUser
             string email = "john.wick@gamil.com";
             string passwordHash = "Hash";
             string role = "Customer";
+
             User user = new (firstName,lastName,email,passwordHash,role);
 
             Assert.Equal(firstName, user.FirstName);
@@ -21,6 +22,37 @@ namespace TestUser
         }
 
         [Fact]
+        public void Create_ShouldFail_EmptyFirstName() 
+        { 
+            string firstName = "";
+            string lastName = "Wick";
+            string email = "john.wick@gamil.com";
+            string passwordHash = "Hash";
+            string role = "Customer";
+
+            var exception = Assert.Throws<ArgumentException>
+                (() => new User(firstName, lastName, email, passwordHash, role));
+
+            Assert.Equal("First-name can not be empty!", exception.Message);
+        }
+
+        [Fact]
+        public void Create_ShouldFail_EmptyLastName()
+        {
+            string firstName = "Jhon";
+            string lastName = "";
+            string email = "john.wick@gamil.com";
+            string passwordHash = "Hash";
+            string role = "Customer";
+
+            var exception = Assert.Throws<ArgumentException>
+                (() => new User(firstName, lastName, email, passwordHash, role));
+
+            Assert.Equal("Last-name can not be empty!", exception.Message);
+        }
+
+
+        [Fact]
         public void Create_ShouldFail_InvalidEmail()
         {
             string firstName = "john";
@@ -28,9 +60,41 @@ namespace TestUser
             string email = "john.wick-gamil.com";
             string passwordHash = "Hash";
             string role = "Customer";
+
             var exception = Assert.Throws<ArgumentException>
                 (() => new User(firstName, lastName, email, passwordHash, role));
+
             Assert.Equal("Invalid email!", exception.Message);
+        }
+
+        [Fact]
+        public void Create_ShouldFail_EmptyPassWord()
+        {
+            string firstName = "john";
+            string lastName = "Wick";
+            string email = "john.wick@gamil.com";
+            string passwordHash = "";
+            string role = "Customer";
+
+            var exception = Assert.Throws<ArgumentException>
+                (() => new User(firstName, lastName, email, passwordHash, role));
+
+            Assert.Equal("Password can not be empty!", exception.Message);
+        }
+
+        [Fact]
+        public void Create_ShouldFail_InvalidRole()
+        {
+            string firstName = "john";
+            string lastName = "Wick";
+            string email = "john.wick@gamil.com";
+            string passwordHash = "Hash";
+            string role = "Worker";
+
+            var exception = Assert.Throws<ArgumentException>
+                (() => new User(firstName,lastName,email,passwordHash,role));
+
+            Assert.Equal("Invalid user role!", exception.Message);
         }
     }
 }
