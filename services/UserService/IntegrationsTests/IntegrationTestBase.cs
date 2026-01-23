092ef3ad2d978afa2c43f4350a0925bafe9f4e20
Alinabi1 <alinabi@kth.se>
2026-01-23 03:04:40 +0100

    a database / IntegrationTest

--- services/UserService/IntegrationsTests/IntegrationTestBase.cs
+++ services/UserService/IntegrationsTests/IntegrationTestBase.cs
@@ -19,7 +19,8 @@ namespace IntegrationsTests
 
             var options = new DbContextOptionsBuilder<AppDbContext>()
                 .UseMySql(connectionString,
-                    new MySqlServerVersion(new Version(8, 0, 26)))
+                    new MySqlServerVersion(new Version(8, 0, 26)),
+                    mySqlOptions => mySqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(2), null))
                 .Options;
 
             var dbContext = new AppDbContext(options);
