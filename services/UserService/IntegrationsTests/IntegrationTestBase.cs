cce362314197499071ba5d9eb77567b145eced5c
Alinabi1 <alinabi@kth.se>
2026-01-23 03:41:21 +0100

    a database / IntegrationTest

--- services/UserService/IntegrationsTests/IntegrationTestBase.cs
+++ services/UserService/IntegrationsTests/IntegrationTestBase.cs
@@ -12,36 +12,22 @@ namespace IntegrationsTests
     {
         protected AppDbContext CreateDbContext()
         {
+            Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
             var databaseName = $"user_service_test_{Guid.NewGuid():N}";
 
             var connectionString =
                 $"Server=mysql;Database={databaseName};User=root;Password=root;";
 
             var options = new DbContextOptionsBuilder<AppDbContext>()
-                .UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 26)),
-                    mySqlOptions => mySqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null))
+                .UseMySql(connectionString,
+                    new MySqlServerVersion(new Version(8, 0, 26)))
                 .Options;
 
-            int retry = 0;
-            int maxRetry = 10;
-            while (true)
-            {
-                try
-                {
-                    var dbContext = new AppDbContext(options);
-                    dbContext.Database.EnsureCreated(); // här försöker EF ansluta
-                    Console.WriteLine($"Creating DB for test: {databaseName}");
-                    return dbContext;
-                }
-                catch (MySqlConnector.MySqlException)
-                {
-                    retry++;
-                    if (retry >= maxRetry)
-                        throw; // ger upp efter 10 försök
-                    Console.WriteLine($"Waiting for MySQL to start... Attempt {retry}/{maxRetry}");
-                    Task.Delay(2000).Wait(); // vänta 2 sek och försök igen
-                }
-            }
+            var dbContext = new AppDbContext(options);
+            dbContext.Database.EnsureCreated();
+            Console.WriteLine($"Creating DB for test  : {databaseName}");
+
+            return dbContext;
         }
 
         protected void CleanupDatabase(AppDbContext context)
