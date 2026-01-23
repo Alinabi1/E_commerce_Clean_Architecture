6175737ae8d8fef2c30661a0b2491fc64a9c4eb3
Alinabi1 <alinabi@kth.se>
2026-01-23 03:34:53 +0100

    a database / IntegrationTest

--- services/UserService/IntegrationsTests/IntegrationTestBase.cs
+++ services/UserService/IntegrationsTests/IntegrationTestBase.cs
@@ -19,14 +19,29 @@ namespace IntegrationsTests
 
             var options = new DbContextOptionsBuilder<AppDbContext>()
                 .UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 26)),
-                    mySqlOptions => mySqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(2), null))
+                    mySqlOptions => mySqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null))
                 .Options;
 
-            var dbContext = new AppDbContext(options);
-            dbContext.Database.EnsureCreated();
-            Console.WriteLine($"Creating DB for test  : {databaseName}");
-
-            return dbContext;
+            int retry = 0;
+            int maxRetry = 10;
+            while (true)
+            {
+                try
+                {
+                    var dbContext = new AppDbContext(options);
+                    dbContext.Database.EnsureCreated(); // här försöker EF ansluta
+                    Console.WriteLine($"Creating DB for test: {databaseName}");
+                    return dbContext;
+                }
+                catch (MySqlConnector.MySqlException)
+                {
+                    retry++;
+                    if (retry >= maxRetry)
+                        throw; // ger upp efter 10 försök
+                    Console.WriteLine($"Waiting for MySQL to start... Attempt {retry}/{maxRetry}");
+                    Task.Delay(2000).Wait(); // vänta 2 sek och försök igen
+                }
+            }
         }
 
         protected void CleanupDatabase(AppDbContext context)
